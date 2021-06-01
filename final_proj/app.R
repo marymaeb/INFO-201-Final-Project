d8409e4596a2c91b2ee8d4b14049539948b41975
Amy Chew <achew20@uw.edu>
2021-06-01 00:28:40 -0700

    fix format

--- final_proj/app.R
+++ final_proj/app.R
@@ -13,75 +13,52 @@ library(ggplot2)
 library(tidyverse)
 library(maps)
 
-# load data
-shoot <- read.csv("fatal-police-shootings-data.csv")
-## provide reference to this data 
-fips_data <- read.csv("https://raw.githubusercontent.com/kjhealy/fips-codes/master/state_fips_master.csv")
+data <- read.csv("fatal-police-shootings-data.csv")
+by_race <- data %>%
+    group_by(race, gender) %>%
+    summarise(shootings = n()) 
 
+# create map of shooting counts 
+## load data
+fips_data <- read.csv("https://raw.githubusercontent.com/kjhealy/fips-codes/master/state_fips_master.csv")
 state_abb_data <- read.csv("https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv")
 state_abb_data$State <- tolower(state_abb_data$State)
 
-# manipulate data 
-shoot_map_data <- shoot %>% 
+## manipulate data 
+shoot_map_data <- data %>% 
     select(id, armed, state, signs_of_mental_illness)
-<<<<<<< HEAD
-# add fips data to shooting data 
-fips_data <- fips_data %>% select(state_abbr, fips)
-shoot_map_data <- left_join(shoot_map_data, fips_data, by = c("state" = "state_abbr"))
-=======
-
+## create counts of shootings by state 
 shoot_count <- shoot_map %>% 
     group_by(Abbreviation) %>% 
     summarize(count = n())
 
-# add fips data to shooting data 
+## add fips data to shooting data 
 fips_data <- fips_data %>% select(state_abbr, fips)
 shoot_map_data <- left_join(shoot_map_data, fips_data, by = c("state" = "state_abbr"))
 
+## add fips data to shooting data 
+fips_data <- fips_data %>% select(state_abbr, fips)
+shoot_map_data <- left_join(shoot_map_data, fips_data, by = c("state" = "state_abbr"))
 
-
-# create state data 
+## create state data 
 state_shapes <- map_data("state")
 state_shapes <- left_join(state_shapes, state_abb_data, by = c("region" = "State"))
 
-# attach state data to shooting data 
+## attach state data to shooting data 
 shoot_map <- left_join(state_shapes, shoot_map_data, by = c("Abbreviation" = "state"))
-# attach counts to shooting and map data 
+## attach counts to shooting and map data 
 shoot_map <- left_join(shoot_map, shoot_count, by = "Abbreviation")
 
-# plot map 
+## plot map 
 shoot_map_plot <- ggplot(shoot_map, aes(long, lat, group = group)) +
     geom_polygon(aes(fill = count)) + coord_quickmap() + 
     labs(
         title = "Count of Fatal Police Shootings by US State"
     )
     
-
 shoot_map_plot
->>>>>>> amy-branch
-
-
-
-# create state data 
-state_shapes <- map_data("state")
-state_shapes <- left_join(state_shapes, state_abb_data, by = c("region" = "State"))
-
-# attach state data to shooting data 
-shoot_map <- left_join(state_shapes, shoot_map_data, by = c("Abbreviation" = "state"))
-
-# plot map 
-shoot_map_plot <- ggplot(shoot_map, aes(long, lat, group = group)) +
-    geom_polygon() + coord_quickmap()
-
 
 server <- function(input, output) {
-    data <- read.csv("fatal-police-shootings-data.csv") 
-    
-    
-    by_race <- data %>%
-        group_by(race, gender) %>%
-        summarise(shootings = n()) 
-    
     gender_name <- reactive({
         if(is.null(input$gender)) {
             by_race
@@ -89,7 +66,6 @@ server <- function(input, output) {
             by_race %>%
                 filter(gender %in% input$gender) 
         }
-        
     }) 
     output$race_bar <- renderPlot({
         ggplot(gender_name(), aes(race, shootings )) +
@@ -99,8 +75,6 @@ server <- function(input, output) {
                 y = "Number of Shootings", 
                 title = "Number of Shootings by Race and Gender") +
             theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5))
-        
-        
     })
     output$message <- renderText ({
         high_race <- by_race %>%
