a6302125dbe8efcc4e8165c3c2c148c549714bd3
Amy Chew <achew20@uw.edu>
2021-06-03 01:38:31 -0700

    finalize map and text

--- final_proj/app.R
+++ final_proj/app.R
@@ -31,16 +31,6 @@ state_abb_data$State <- tolower(state_abb_data$State)
 ## manipulate data 
 shoot_map_data <- data %>% 
     select(id, armed, state, signs_of_mental_illness)
-## create counts of shootings by state 
-shoot_count <- shoot_map %>% 
-    group_by(Abbreviation) %>% 
-    summarize(count = n())
-armed_count <- shoot_map %>% 
-    group_by(armed) %>% 
-    summarize(count = n()) %>% arrange(desc(count))
-armed_count <- armed_count[-4,]
-## reduce to top ten 
-armed_count <- armed_count[1:10,]
 
 ##find highest shoot count 
 shoot_count_highest <- shoot_count %>%
@@ -72,6 +62,41 @@ shoot_map <- shoot_map %>%
     rename(
         armed_count = count
     )
+
+prop_ill_true <- shoot_map %>% 
+  filter(signs_of_mental_illness == "True") %>% 
+  group_by(Abbreviation) %>% 
+  summarize(true_count = n())
+
+prop_ill_false <- shoot_map %>% 
+  filter(signs_of_mental_illness == "False") %>% 
+  group_by(Abbreviation) %>% 
+  summarize(false_count = n())
+
+shoot_map <- left_join(shoot_map, prop_ill_false, by = "Abbreviation")
+shoot_map <- left_join(shoot_map, prop_ill_true, by = "Abbreviation")
+
+prop_ill_dat <- shoot_map %>% 
+  select(false_count, true_count, long, lat, group, 
+         Abbreviation, signs_of_mental_illness, fips.x, fips.y, state_count) %>% 
+  group_by(Abbreviation) %>% 
+  mutate(prop_true = (true_count / state_count) * 100,
+         prop_false = (false_count / state_count) * 100)
+
+## create counts of shootings by state 
+shoot_count <- shoot_map %>% 
+    group_by(Abbreviation) %>% 
+    summarize(count = n())
+armed_count <- shoot_map %>% 
+    group_by(armed) %>% 
+    summarize(count = n()) %>% arrange(desc(count))
+armed_top_ten <- armed_count[-4,]
+## reduce to top ten 
+armed_top_ten <- armed_top_ten[1:10, 1]
+armed_top_ten <- armed_top_ten$armed
+
+## filter shoot data to only these top ten armed 
+shoot_map <- shoot_map %>% filter(armed %in% armed_top_ten)
 
 ## plot map 
 shoot_map_plot <- ggplot(shoot_map, aes(long, lat, group = group)) +
@@ -118,14 +143,24 @@ server <- function(input, output) {
 ##################End Mary-Mae Page
  ###################AmyPage
     output$map <- renderPlot({
-        #subset <- shoot_map %>% 
-            #filter(count)
-        shoot_map_plot <- ggplot(shoot_map, aes(long, lat, group = group)) +
-            geom_polygon(aes(fill = count)) + coord_quickmap() + 
+       # subset <- prop_ill_dat %>% 
+            #filter(armed %in% input$armed)
+        shoot_map_plot <- ggplot(prop_ill_dat, aes(long, lat, group = group)) +
+            geom_polygon(aes(fill = prop_true)) + coord_quickmap() + 
             labs(
-                title = "Count of Fatal Police Shootings by US State"
-            )
-        
+                title = "Proportion of Individuals Fatally Shot and Mentally-Ill, by State"
+            ) +  scale_fill_gradient2("Proportion of Total Shot")
+        shoot_map_plot
+    })
+    
+    output$map_descript <- renderText({
+      print("This map plots the proportion of individuals fatally shot by police that were mentally ill. 
+            States colored in with a lighter shade of purple have higher proportions than states colored 
+            with a darker purple The plot suggests that states with the highest proportions of mentally ill 
+            victims of police shootings were Wyoming, South Dakota, and Vermont. The states with the lowest
+            proportions were Kentucky, Maine, and Montana. Overall, the map suggests that states in the 
+            southern parts of the US have higher proportions of mentally ill individuals shot by policemen
+            than northern states.")
     })
 ###################End Amy Page
     ##################AshleyPage
@@ -138,19 +173,27 @@ server <- function(input, output) {
 ###################End Ashley Page
  #############conclusion
     output$conclusion <- renderText({
-        print("Throughout our project we looked at a lot of different aspects of this data. 
-              To start, we look at the shooting counts among the different states. We found that ", shoot_count_high$Abbreviation[1], 
-              " had the highest total shoot count with ", shoot_count_high$count[1], " fatal police shootings.
-              We allow the user to switch between different things the person was armed (or not armed with) this showed a pattern that 
-              a high majority of people who were shot were actually not armed. This number is alarming with ", armed_count$count[3], 
-              " people being unarmed while they were fatally shot by the police. This shows that many police error on the side of caution
-              and fear when shooting at citzens, which can be very problematic when it entails taking a human life.")
+        # print("Throughout our project we looked at a lot of different aspects of this data. 
+        #       To start, we look at the shooting counts among the different states. We found that ", shoot_count_high$Abbreviation[1], 
+        #       " had the highest total shoot count with ", shoot_count_high$count[1], " fatal police shootings.
+        #       We allow the user to switch between different things the person was armed (or not armed with) this showed a pattern that 
+        #       a high majority of people who were shot were actually not armed. This number is alarming with ", armed_count$count[3], 
+        #       " people being unarmed while they were fatally shot by the police. This shows that many police error on the side of caution
+        #       and fear when shooting at citzens, which can be very problematic when it entails taking a human life.")
+        print("Throughout our project, we aimed to examine aspects of the data that would provide insight to the characteristics 
+        of the victims of police shootings. The first panel displays a USmap which is colored according to the proportion of individuals 
+              who were fatally shot by policemen and were mentally ill (out of all individuals shot). This map gave evidence to suggest 
+              states in the south had higher proportions of mentally ill individuals who were victimized, while states in the north had 
+              a smaller proportion. This evidence suggests that there is a disproportionate approach as to how US policing systems issue order
+              when confronting an individual with a mental illness. This suggests that nationwide training should be instilled in police forces
+              to detail how to respond to calls involving mentally ill individuals. It is then that we can hope to reduce these numbers.") 
         br()
         print("In our next page, we showed a bar graph that showed the number of fatal shootings spiliting up by race. This showed us that in this data, the most people
-              being shot were the ", by_race_high$race[1], " race and ", by_race_high$gender[1], " gender with ", by_race_high$shootings[1], 
+              being shot were the ", by_race_high$race[1], " race and ", by_race_high$gender[1], " gender with ", by_race_high$shootings[1],
               " shootings. This number is closely followed by Black Males with ", by_race_high$shootings[2], " shootings. This shows us
-              that Males are being shot more than females and in this particular data White Males more than Blakc Males. In our last page we 
+              that Males are being shot more than females and in this particular data White Males more than Blakc Males. In our last page we
               created a bar graph that depicted that manner of death.")
+        
         br()
         print("ADD ASHLEY DATA!!!! All of this data is very important because it allows us to investigate the Police force and think about the manner of fatal shootings. Are they always justified? With these 
               numbers we can see that although more people are armed than unarmed, there is still an alarming number of unarmed 
@@ -169,7 +212,6 @@ server <- function(input, output) {
     
 }
 
-
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     tabsetPanel(type = "tabs",
@@ -181,9 +223,9 @@ ui <- fluidPage(
                 tabPanel("Tab 2", 
                          h2("Amy Page")
                 ), 
-                # selectInput("armed", "weapon", choices = unique(shoot_map$armed))
                 mainPanel(
-                    plotOutput("map")
+                    plotOutput("map"),
+                    textOutput("map_descript")
                 ),
                 tabPanel("Tab 2",
                          h2("Mary-Mae Page") 
@@ -210,17 +252,16 @@ ui <- fluidPage(
     tabPanel("Tab 4",
              h2("Ashley Page")
     ),
-    sidebarLayout(
-        sidebarPanel(
-            uiOutput("gender"), 
-            selectInput(inputId = "gender", "Select Gender:", 
-                        c("Female" = "F", 
-                          "Male" = "M")), 
-            radioButtons(inputId = "Color", label = "Plot Color", 
-                         c("Red"), 
-                         selected = "Red"
-            )
-        ),
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         uiOutput("gender"), 
+    #         selectInput(inputId = "gender", "Select Gender:", 
+    #                     c("Female" = "F", 
+    #                       "Male" = "M")), 
+    #         radioButtons(inputId = "Color", label = "Plot Color", 
+    #                      c("Red"), 
+    #                      selected = "Red")
+    #         ),
         mainPanel(
             plotOutput("mannerBar")
         ),
@@ -232,8 +273,8 @@ ui <- fluidPage(
         mainPanel(
             textOutput("conclusion")
         )
-    
 )
-)
+
           
 shinyApp(ui = ui , server = server)
+
